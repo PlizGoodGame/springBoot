49a72f9142f36def0deb6675bfbdbbed5189d776
PlizGoodGame <artemkruglov026@gmail.com>
2026-04-12 21:08:25 +0300

    patterns

--- src/main/java/com/example/kafka/KafkaProducerService.java
+++ src/main/java/com/example/kafka/KafkaProducerService.java
@@ -1,6 +1,9 @@
 package com.example.kafka;
 
 import com.example.event.UserEvent;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 import org.springframework.stereotype.Service;
 import org.springframework.kafka.core.KafkaTemplate;
 
@@ -13,7 +16,12 @@ public class KafkaProducerService {
         this.kafkaTemplate = kafkaTemplate;
     }
 
+    @CircuitBreaker(name = "kafkaService", fallbackMethod = "fallback")
     public void send(UserEvent event) {
         kafkaTemplate.send("user-topic", event);
+    }
+
+    public void fallback(UserEvent event, Throwable t) {
+        System.out.println("Kafka недоступна, событие не отправлено: " + event.getEmail());
     }
 }
--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -65,4 +65,6 @@ public class UserService {
         user.setCreatedAt(dto.getCreatedAt());
         return user;
     }
+
+    
 }
